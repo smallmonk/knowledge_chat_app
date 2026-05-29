0caf2e2b1eb45ab2e9b9b3802a2f012adb8c4caf
smallmonk <phuaytmy@gmail.com>
2026-05-29 08:57:18 +0800

    Allow CORS for "python_qa_rag_api"

--- python_qa_rag_api/app/main.py
+++ python_qa_rag_api/app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from .indexer import load_vector_index
 from .routes import router
 
 app = FastAPI(title="Vector RAG Knowledge Base Q&A Bot")
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(router)
 
 
